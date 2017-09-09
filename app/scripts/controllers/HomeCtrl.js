ff535423bcb589f5c2f7058ead6065e817bc21b1
Ayffi Nawaz <ayffin@gmail.com>
2017-09-08 21:17:43 -0400

    list-message checkpoint completed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,17 @@
 (function() {
-  function HomeCtrl(Room) {
+  function HomeCtrl($scope, Message, Room) {
     this.rooms = Room.all;
+    this.changeRoom = function(room) {
+      $scope.activeRoom = room;
+      console.log($scope.activeRoom);
+      $scope.messages = Message.getByRoomId($scope.activeRoom.$id);
+
+    };
+
+
   }
 
   angular
     .module('blocChat')
-    .controller('HomeCtrl', ['Room', HomeCtrl]);
+    .controller('HomeCtrl', ["$scope", 'Message', 'Room', HomeCtrl]);
 })();
